88584435d94c45f0ee9dfbd88d5ebc2464fbba2f
Dominic Whelan <dominic.whelan27@gmail.com>
2023-03-10 10:45:48 -0330

    Exercise 5 completed

--- src/main/java/_06_05/FileReaderExercise.java
+++ src/main/java/_06_05/FileReaderExercise.java
@@ -1,20 +1,34 @@
 package _06_05;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class FileReaderExercise {
 
     // This method should return the first line of BufferedReaderVsScanner.txt.
     String getFirstLine() {
-        String firstLine = "";
-
-        return firstLine;
+        try(BufferedReader reader = new BufferedReader(new FileReader("src/main/java/_06_05/BufferedReaderVsScanner.txt"))) {
+            return reader.readLine();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     // This method should return all of the content of BufferedReaderVsScanner.txt
     // as a single String.
     String getWholeFile() {
-        String wholeFile = "";
+        try(BufferedReader reader = new BufferedReader(new FileReader("src/main/java/_06_05/BufferedReaderVsScanner.txt"))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            reader.lines().forEach(stringBuilder::append);
+            return stringBuilder.toString();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
 
-        return wholeFile;
     }
 
 }
